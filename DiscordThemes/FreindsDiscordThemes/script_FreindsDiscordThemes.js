2fb4ffe9a1d4708d4ae02d1d32d708aca21a8fe8
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2019-08-15 17:52:15 -0400

    Update script_FreindsDiscordThemes.js

--- DiscordThemes/FreindsDiscordThemes/script_FreindsDiscordThemes.js
+++ DiscordThemes/FreindsDiscordThemes/script_FreindsDiscordThemes.js
@@ -2,64 +2,64 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 100,
+      "value": 200,
       "density": {
         "enable": true,
         "value_area": 700 } },
 
 
     "color": {
-      "value": "#7bf542" },
+      "value": "#003cff" },
 
     "shape": {
       "type": "circle",
       "stroke": {
-        "width": 0,
+        "width": 1,
         "color": "#ff0022" },
 
       "polygon": {
-        "nb_sides": 5 } },
+        "nb_sides": 100 } },
 
 
     "opacity": {
-      "value": 0.5,
-      "random": false,
+      "value": 0.6,
+      "random": true,
       "anim": {
-        "enable": false,
-        "speed": 1,
+        "enable": true,
+        "speed": 5,
         "opacity_min": 0.1,
         "sync": false } },
 
 
     "size": {
-      "value": 3,
+      "value": 7,
       "random": true,
       "anim": {
         "enable": false,
-        "speed": 40,
+        "speed": 90,
         "size_min": 0.1,
         "sync": false } },
 
 
     "line_linked": {
       "enable": true,
-      "distance": 150,
-      "color": "#ff0022",
+      "distance": 200,
+      "color": "#085e61",
       "opacity": 0.4,
-      "width": 1 },
+      "width": 2 },
 
     "move": {
       "enable": true,
-      "speed": 10,
+      "speed": 40,
       "direction": "none",
-      "random": false,
+      "random": true,
       "straight": false,
       "out_mode": "out",
       "bounce": true,
       "attract": {
         "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200 } } },
+        "rotateX": 100,
+        "rotateY": 10 } } },
 
 
 
@@ -85,7 +85,7 @@ particlesJS("particles-js", {
 
       "bubble": {
         "distance": 400,
-        "size": 90,
+        "size": 100,
         "duration": 2,
         "opacity": 8,
         "speed": 3 },

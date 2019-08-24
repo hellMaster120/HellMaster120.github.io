b0eeb633992e8d117d4a08a5b77c819349ec43ac
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2019-08-24 19:05:30 -0400

    .

---
+++ Js/JS.js
@@ -0,0 +1,22 @@
+function getInfo() {
+	var password = document.getElementById('password').value
+
+	
+		if(password == "611124") {
+			console.log("Loging in.....")
+
+		
+	document.getElementById('Thing').style.opacity = "0"	
+	document.getElementById('password').style.right = "300%"
+	document.getElementById('button').style.right = "300%"
+			return
+}
+
+console.log("incorrect username or password")
+}
+
+function back() {
+	document.getElementById('Thing').style.opacity = "0"	
+	document.getElementById('password').style.right = "50%"
+	document.getElementById('button').style.right = "50%"
+}

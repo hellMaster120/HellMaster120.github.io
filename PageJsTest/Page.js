5b073dd8ccfb30abe046a9a474f9191ce379c977
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2019-08-26 19:00:15 -0400

    /

--- PageJsTest/Page.js
+++ PageJsTest/Page.js
@@ -1,4 +1,3 @@
-
 function Right(Index,Number){
 document.getElementById(''+Index.toString()+'').style.right = ""+Number+"%"
 }
@@ -41,33 +40,4 @@ Top(Index2.toString(),Top_Number)
 Opacity(Index1.toString(),Opacity_Number1)
 Opacity(Index2.toString(),Opacity_Number2)
 console.log("Page "+Page)
-}
-
-
-
-function getInfo() {
-	var password = document.getElementById('password').value
-
-	
-		if(password == "611124" || password == "621124") {
-			console.log("Loging in...")
-	
-
-
-
-PageTop_To_Bottom("D","Thing",50,-126,100,0,1) 
-			return
-}
-
-console.log("incorrect username or password")
-}
-
-
-function back() {
-	
-	document.getElementById('D').style.opacity = "0"
-	document.getElementById('D').style.top = "-70%"
-	document.getElementById('Thing').style.opacity = "100"	
-	document.getElementById('Thing').style.top = "50%"	
-
 }

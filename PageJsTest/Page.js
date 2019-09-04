3a4d91eccd05aaeb6fd62a3ca6e004e11c70b842
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2019-09-04 01:51:51 -0400

    /

--- PageJsTest/Page.js
+++ PageJsTest/Page.js
@@ -47,6 +47,7 @@ console.log(I)
 function Page(){
 console.log(I)
 }
+
 function Right(Index,Number){
 document.getElementById(''+Index.toString()+'').style.right = ""+Number+"%"
 }
@@ -61,4 +62,28 @@ document.getElementById(''+Index.toString()+'').style.bottom = ""+Number+"%"
 }
 function Opacity(Index,Number) {
 document.getElementById(''+Index.toString()+'').style.opacity = ""+Number+""
+}
+function PageLeft_To_Right(Index1,Index2,Left_Number1,Right_Number2,Opacity_Number1,Opacity_Number2) {
+Left(Index1.toString(),Left_Number1)
+Right(Index2.toString(),Right_Number2)
+Opacity(Index1.toString(),Opacity_Number1)
+Opacity(Index2.toString(),Opacity_Number2)
+}
+function PageRight_To_Left(Index1,Index2,Right_Number,Left_Number,Opacity_Number1,Opacity_Number2) {
+Right(Index1.toString(),Right_Number)
+Left(Index2.toString(),Left_Number)
+Opacity(Index1.toString(),Opacity_Number1)
+Opacity(Index2.toString(),Opacity_Number2)
+}
+function PageTop_To_Bottom(Index1,Index2,Top_Number,Bottom_Number,Opacity_Number1,Opacity_Number2) {
+Top(Index1.toString(),Top_Number)
+Bottom(Index2.toString(),Bottom_Number)
+Opacity(Index1.toString(),Opacity_Number1)
+Opacity(Index2.toString(),Opacity_Number2)
+}
+function PageBottom_To_Top(Index1,Index2,Bottom_Number,Top_Number,Opacity_Number1,Opacity_Number2) {
+Bottom(Index1.toString(),Bottom_Number)
+Top(Index2.toString(),Top_Number)
+Opacity(Index1.toString(),Opacity_Number1)
+Opacity(Index2.toString(),Opacity_Number2)
 }

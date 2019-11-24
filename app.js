394f6b8fb1e46be3654d4f1c9aceabe3e5ac3022
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2019-11-24 00:54:11 -0500

    e

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/',function(req,res) {
 	res.sendFile(__dirname + '/index.html');
 });
 app.use('/index',express.static(__dirname+'/index'));
-serv.listen(8443)
+serv.listen(443)
 console.log('server stared')
 
 var io = require('socket.io')(serv,{});

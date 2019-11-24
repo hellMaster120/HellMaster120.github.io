69d77de9027c7eed7c6a7a633ed4fa6afa07a3ea
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2019-11-24 01:09:38 -0500

    e

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express()
 var serv = require('http').Server(app);
+var port = normalizePort(process.env.PORT || '3000')
+app.set('port',port)
+
 app.get('/',function(req,res) {
 	res.sendFile(__dirname + '/index.html');
 });
 app.use('/index',express.static(__dirname+'/index'));
-serv.listen(443)
+
+
+serv.listen(port)
+
 console.log('server stared')
 
 var io = require('socket.io')(serv,{});

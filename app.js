26f1d28efe94ed221b6e9a1f19d8fdb48b584349
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2019-11-23 21:21:37 -0500

    e

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.urlencoded({extended : false}))
+
+//server static files
+app.use(express.static(path.join(__dirname,'public')))
+
+//template engine 
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','pug')
+
+//setting up the server
+
+app.listen(3000,()=> {
+
+	console.log('server is runing on ')
+})
+module.exports = app

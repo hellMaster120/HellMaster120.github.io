d6ab7bc4fa1dec4066650c6400fe0423dfdd3940
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2019-11-24 00:10:03 -0500

    e

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var express = require('express')
 var app = express()
-var serv = require('http').Server(app)
-app.use(express.urlencoded({extended : false}))
+var serv = require('http'),Server(app)
 
-
-app.get('/',function(req,res){
-	res.sendFile(__dirname+"index.html")
+app.get('/',function(req,res) {
+	res.sendFile(__dirname+'index.html')
 })
 app.use('client',express.static(__dirname+'client'))
 
-//serv.listen(2000)
+console.log("Server started")
+
 var io = require('socket.io')(serv,{})
 io.sockets.on('connection',function(socket) {
-		console.log("socket connection")
+		console.log('socket connection')
 })

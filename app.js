552c63f72666f859f4cae009fc69404819514a3d
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2019-11-23 22:29:13 -0500

    e

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const express = require('express')
 const path = require('path')
-
 const app = express()
 
 app.use(express.urlencoded({extended : false}))
 
 //server static files
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'/client')))
 
-//template engine 
-app.set('views',path.join(__dirname,'views'))
-app.set('view engine','pug')
 //le's server the index page
 app.get('/',function(req,res){
-	res.render('index')
-
-
-
+	res.sendFile(__dirname+"/client/index.html")
 })
-
-
+app.use('/client',express.static(__dirname+'/client'))
 //setting up the server
 
 app.listen(3000,()=> {

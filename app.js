9a7ee9e1b8035cd941408d19026e0b5199153751
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2019-11-23 23:47:40 -0500

    e

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname,'/client')))
 
 //le's server the index page
 app.get('/',function(req,res){
-	res.sendFile(__dirname+"index.html")
+	res.sendFile(__dirname+"/client/index.html")
 })
 app.use('/client',express.static(__dirname+'/client'))
 //setting up the server

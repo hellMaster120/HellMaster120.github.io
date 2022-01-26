96e4e4ac45823c1826e612e36ff6a8c0361e6ea6
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2022-01-26 07:44:45 -0500

    .

--- JS/Particles.js
+++ JS/Particles.js
@@ -18,7 +18,7 @@ let II;
 
 function setup(){
 createCanvas(window.innerWidth,window.innerHeight);
-MaxParticles = Math.floor(height/10)
+MaxParticles = Math.floor(height/6)
 II = 0
 for(let i = 0;i<MaxParticles;i++){
 Particles.push(new Particle)  
